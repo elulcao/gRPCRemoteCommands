795e9530009f7da5cb4a180f20be4340c7cb2e92
elulcao <elulcao@icloud.com>
2021-12-29 08:59:57 -0600

    Add main.go and go.*

---
+++ main.go
@@ -0,0 +1,12 @@
+/*
+Copyright © 2021 elulcao
+*/
+
+package main
+
+import "github.com/elulcao/gRPCRemoteCommands/cmd"
+
+// main is the entry point for the application.
+func main() {
+	cmd.Execute()
+}
